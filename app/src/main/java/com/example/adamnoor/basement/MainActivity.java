74f50a27bcc9b073810909803cf3ed9a20abbab6
DeveloperAdam <adamnooriit@gmail.com>
2017-09-12 15:31:25 +0500

    Initial commit

---
+++ app/src/main/java/com/example/adamnoor/basement/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.adamnoor.basement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button
+
+    @Override btnSignUp
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
